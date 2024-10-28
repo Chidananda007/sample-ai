cf083f058d686cbcb5b7d45d76ef76c5bb8076e5
chida <chidananda676@gmail.com>
2024-10-28 20:26:26 +0530

    ai demo project

---
+++ src/main/java/com/example/sample_ai/SampleAiController.java
@@ -0,0 +1,18 @@
+package com.example.sample_ai;
+
+import com.example.sample_ai.assignment.SampleAiService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/open-ai")
+public class SampleAiController {
+
+    private final SampleAiService aiService;
+
+    @GetMapping("/languages/{language}/translator")
+    public  String translate(@RequestParam("text") String text,@PathVariable("language") String language){
+        return aiService.translate(language,text);
+    }
+}
---
+++ src/main/java/com/example/sample_ai/assignment/SampleAiService.java
@@ -0,0 +1,14 @@
+package com.example.sample_ai.assignment;
+
+import dev.langchain4j.service.SystemMessage;
+import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.V;
+import dev.langchain4j.service.spring.AiService;
+
+@AiService
+public interface SampleAiService {
+    @SystemMessage("Your are a professional translator into {{language}}")
+    @UserMessage("translate the following text {{text}}")
+    String translate(@V("language") String language,
+                     @V("text") String text);
+}
